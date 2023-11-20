9d5f5f14b7419a0120a6da6fe9a55ff1f63e3e72
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-11-20 18:47:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 let element=document.createElement("spain");
-element.innertext="DOM load success"
+element.innerText=abvvbnbvnb;
 const body_ele=document.querySelector("body");
 body_ele.appendChild("element");
